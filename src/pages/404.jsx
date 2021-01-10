87b13b1c50fe33cc74953467bcc49f3e4922fa3c
JasonSooter <7215306+functionalStoic@users.noreply.github.com>
2021-01-10 12:15:32 -0700

    feat(nav): add 404 page

---
+++ src/pages/404.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function NotFoundPage() {
+  return (
+    <h1 style={{ margin: '12px', padding: '12px' }}>
+      Page Not Found
+      <br />
+      <a href="/">Go back home -></a>
+    </h1>
+  );
+}
+export default NotFoundPage;
