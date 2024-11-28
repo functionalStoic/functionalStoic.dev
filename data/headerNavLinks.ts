6aa875c3b6ccd55d1a2d51f7c39ecd071b807e71
functionalStoic <7215306+functionalStoic@users.noreply.github.com>
2024-11-28 11:58:32 -0700

    feat(code): add header link to my notes

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,6 +1,7 @@
 const headerNavLinks = [
   { href: '/', title: 'Home' },
   { href: '/about', title: 'About' },
+  { href: '/my-notes', title: 'My Notes' },
   { href: '/blog', title: 'Blog' },
   { href: '/blog/uses', title: 'Uses' },
   { href: '/blog/bookshelf', title: 'Bookshelf' },
